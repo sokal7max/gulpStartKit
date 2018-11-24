89e76315201af92ae4cedcf3f4afd74cae195628
Max Sokal <sokol.max777@gmail.com>
2018-11-24 21:51:51 +0300

    First commit

---
+++ app/src/js/main.js
@@ -0,0 +1,8 @@
+(function () {
+	$(".alert").click(function () {
+        2
+            $(this).css({'backgroundColor' : '#000', 'color':'#fff', 'padding':'5px'});
+        3
+        });
+        
+}());
